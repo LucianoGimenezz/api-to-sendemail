e1b1989103fb80400c85dbc3f6924cb565cf34dd
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2023-08-24 15:11:13 -0300

    change to nullshi

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ dotenv.config()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT ?? 3001
 const app = express()
 
 app.use(express.json())
